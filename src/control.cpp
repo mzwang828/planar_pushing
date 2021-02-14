bb2da938047fe85d46bacdc62e33aba6907cd313
mzwang <mao@maogic.me>
2021-02-13 19:19:39 -0500

    define ref traj

--- src/control.cpp
+++ src/control.cpp
@@ -6,6 +6,7 @@
 
 #include <yaml-cpp/yaml.h>
 #include <math.h>
+#include <algorithm>
 #include <ifopt/problem.h>
 #include <ifopt/snopt_solver.h>
 #include "pusher/problem.h"
@@ -43,10 +44,32 @@ public:
             state.segment(i*4, 4) << 0.05*i*tStep, 0,0,0;
             control.segment(i*3, 3) << 0.3, 0.0, 0.0;
         }
-        
+
+        stepCounter = 0;
+
+        // define the nominal trajectory
+        float targetLV = 0.05;
+        lineNomi.resize(16000);
+        for (int i = 0; i < 10/(targetLV*tStep); ++i){
+            lineNomi.segment(i*4, 4) << targetLV*i*tStep, 0, 0, 0;
+        }
+
         // circular tracking
-        radius = 0.20;
-        alphaDot = 0.05/(2*3.14*radius);
+        radius = 0.16;
+        float targetAV = 0.3;
+        int nPointsPi = 3.14/(targetAV*tStep) + 1;        
+        eightNomi.resize(nPointsPi*4*4);
+        for (int i = 0; i < nPointsPi-1; ++i){
+            eightNomi.segment(i*4, 4) << radius * sin(targetAV*i*tStep), radius - radius*cos(targetAV*i*tStep), targetAV*i*tStep, 0;
+            eightNomi.segment(4*nPointsPi + i*4, 4) << -radius * sin(targetAV*i*tStep), 3*radius - radius*cos(targetAV*i*tStep), 3.14 - targetAV*i*tStep, 0;
+            eightNomi.segment(2*4*nPointsPi + i*4, 4) << radius * sin(targetAV*i*tStep), 3*radius + radius*cos(targetAV*i*tStep), - targetAV*i*tStep, 0;
+            eightNomi.segment(3*4*nPointsPi + i*4, 4) << -radius * sin(targetAV*i*tStep), radius + radius*cos(targetAV*i*tStep), -3.14 + targetAV*i*tStep, 0;
+        }
+        eightNomi.segment((nPointsPi-1)*4, 4) << 0, 2*radius, 3.14, 0;
+        eightNomi.segment((2*nPointsPi-1)*4, 4) << 0, 4*radius, 0, 0;
+        eightNomi.segment((3*nPointsPi-1)*4, 4) << 0, 2*radius, -3.14, 0;
+        eightNomi.segment((4*nPointsPi-1)*4, 4) << 0, 0, 0, 0;
+
 
         timer1 = nh.createTimer(ros::Duration(0.05), &Control::findSolution, this);
     }
@@ -68,28 +91,26 @@ public:
         sliderPose.theta = tf::getYaw(pose.getRotation());
     }
     
-    /**
-     * Pass in initial guess state and control, the nominal state and control and solve the problem
-     **/
     void findSolution(const ros::TimerEvent&)
     {
         Eigen::VectorXd stateNominal(MPCSteps*4), controlNominal(MPCSteps*3);
 
         // straight line
         for (int i = 0; i < MPCSteps; ++i){
-            stateNominal.segment(i*4, 4) << sliderPose.x + 0.05*i*tStep, 0,0,0;
+            // stateNominal.segment(i*4, 4) << sliderPose.x + 0.05*i*tStep, 0,0,0;
             controlNominal.segment(i*3, 3) << 0.0, 0.0, 0.0;
         }
+        // stateNominal = lineNomi.segment(stepCounter*4, MPCSteps*4);
+        stateNominal = eightNomi.segment(stepCounter*4, MPCSteps*4);
 
         // circular
         // double alpha = atan2(sliderPose.x, radius - sliderPose.y);
         // for (int i = 0; i < MPCSteps; ++i){
-        //     stateNominal.segment(i*4, 4) << radius*sin(alpha + alphaDot*i*tStep), radius - radius*cos(alpha + alphaDot*i*tStep), alpha, 0;
+        //     stateNominal.segment(i*4, 4) << radius*sin(alpha + alphaDot*i*tStep), radius - radius*cos(alpha + alphaDot*i*tStep), alpha + alphaDot*i*tStep, 0;
         //     controlNominal.segment(i*3, 3) << 0.3, 0.0, 0.0;
         // }
         
-        Eigen::VectorXd stateInit = stateNominal;
-        stateInit.segment(0,4) << sliderPose.x, sliderPose.y, sliderPose.theta, phi;
+        stateNominal.segment(0,4) << sliderPose.x, sliderPose.y, sliderPose.theta, phi;
         state.segment(0,4) << sliderPose.x, sliderPose.y, sliderPose.theta, phi;
 
         std::cout << sliderPose.x << ", " << sliderPose.y << ", " << sliderPose.theta << ", " << phi << "\n";
@@ -135,44 +156,9 @@ public:
         // std::cout << alpha << ", " << alpha + alphaDot*19*tStep << ", " << sliderPose.theta << "\n";
         std::cout << "---------\n";
 
+        ++stepCounter;
         pusherVelPub.publish(pusherVel);
     }
-
-    // void straightLine(const ros::TimerEvent&)
-    // {
-    //     Eigen::VectorXd stateNominal(MPCSteps*4), controlNominal(MPCSteps*3);
-    //     for (int i = 0; i < MPCSteps; ++i){
-    //         stateNominal.segment(i*4, 4) << sliderPose.x + 0.05*i*tStep, 0,0,0;
-    //         controlNominal.segment(i*3, 3) << 0.3, 0.0, 0.0;
-    //     }
-
-    //     this->findSolution(state, control, stateNominal, controlNominal);
-
-    //     // convert pusher velocity from slider frame to world frame
-    //     geometry_msgs::Twist pusherVel, zeroVel;
-    //     zeroVel.linear.x = 0.0;
-    //     zeroVel.linear.y = 0.0;
-
-    //     J << 1, 0, -yC,
-    //         0, 1, xC;
-    //     B.col(0) = J.transpose() * Eigen::Vector2d(1, 0);
-    //     B.col(1) = J.transpose() * Eigen::Vector2d(0, 1);
-    //     Eigen::MatrixXd Gc(2,3);
-    //     Gc.leftCols(2) = J * L * B;
-    //     Gc.col(2) << 0.0, -xC/(cos(phi)*cos(phi));
-    //     Eigen::Vector2d vPusher = Gc * control.segment(3, 3);
-    //     pusherVel.linear.x = cos(sliderPose.theta) * vPusher(0) - sin(sliderPose.theta) * vPusher(1);
-    //     pusherVel.linear.y = sin(sliderPose.theta) * vPusher(0) + cos(sliderPose.theta) * vPusher(1);
-
-    //     std::cout << control(3) << ", " << control(4) << ", " << control(5) << "\n";
-    //     std::cout << vPusher(0) << ", " << vPusher(1) << "\n";
-    //     std::cout << "---------\n";
-    //     // getchar();
-
-    //     pusherVelPub.publish(pusherVel);
-    //     // ros::Duration(tStep).sleep();
-    //     // pusherVelPub.publish(zeroVel);
-    // }
 
 private:
     ros::NodeHandle nh;
@@ -182,7 +168,7 @@ private:
 
     geometry_msgs::Pose2D sliderPose;
     double xC, yC, phi, fMax, mMax, c, mu, m, muGround;
-    int MPCSteps;
+    int MPCSteps, stepCounter;
     double tStep;
     double radius;
     double alphaDot;
@@ -191,6 +177,9 @@ private:
     
     // Used to map force to velocity
     Eigen::MatrixXd J, L, B;
+
+    // reference trajectory
+    Eigen::VectorXd lineNomi, eightNomi;
 
     ifopt::SnoptSolver solver;
 };
